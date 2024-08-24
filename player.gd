f6a71d195affc98ab46f2e6d7affc639dd837e9a
PixelPulse283 <pixelpulse283@gmail.com>
2024-08-23 22:52:59 -0500

    Update player.gd

--- player.gd
+++ player.gd
@@ -26,9 +26,12 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = ($Head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
