acbe9c03e67ed97318d0394588708f6a869df8bd
PixelPulse283 <pixelpulse283@gmail.com>
2024-08-23 22:51:51 -0500

    Add files via upload
    Basic Player Movement Code

---
+++ player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+
+const SPEED = 4.4
+const JUMP_VELOCITY = 4.5
+var mouse_sense = 0.005
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		$Head.rotate_y(-event.relative.x * mouse_sense)
+		$Head/Camera3D.rotate_x(-event.relative.y * mouse_sense)
+		$Head/Camera3D.rotation.x = clamp($Head/Camera3D.rotation.x, deg_to_rad(-75), deg_to_rad(60))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = ($Head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
